0926ffd55eadd59ae721582b14d4265c880fb93c
Lazyness <leniviy.demon.1@gmail.com>
2022-12-22 21:39:41 +0200

    update textToSpeect, code into thread because we want to hear voice and swipe text.

--- src/main/java/logic/speech/TextToSpeech.java
+++ src/main/java/logic/speech/TextToSpeech.java
@@ -4,14 +4,16 @@ import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
 public class TextToSpeech {
-    public static void textToSpeech(String nameVoice, String partText) {
-        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
-        // VoiceManager uses the singleton approach to creating and
-        // providing an instance
-        VoiceManager vm = VoiceManager.getInstance();
-        Voice voice = vm.getVoice(nameVoice);
-        voice.allocate();
-        voice.speak(partText);
-        voice.deallocate();
+    public synchronized static void textToSpeech(String nameVoice, String partText) {
+        new Thread(() -> {
+            System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+            // VoiceManager uses the singleton approach to creating and
+            // providing an instance
+            VoiceManager vm = VoiceManager.getInstance();
+            Voice voice = vm.getVoice(nameVoice);
+            voice.allocate();
+            voice.speak(partText);
+            voice.deallocate();
+        }).start();
     }
 }
